ab0005c27a37779bd23282e03fd725d1005b3223
Inessouai <155246475+Inessouai@users.noreply.github.com>
2025-12-11 14:28:02 +0100

    UPDATE

--- backend_api/app.py
+++ backend_api/app.py
@@ -20,7 +20,7 @@ CORS(app)
 # Database configuration
 # -------------------------------------------------------------------
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASS = os.getenv("DB_PASS", "inessouai2005_")
+DB_PASS = os.getenv("DB_PASS", "hatiyourpassword")
 DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME", "password_guardian")
